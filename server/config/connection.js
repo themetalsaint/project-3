01b39fe6a8a849f293d814b42be17125e6f7b781
SpeedRacer <robertgeorge2@gmail.com>
2021-09-09 20:58:59 -0400

    some kind of message 2

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/programming-thoughts',
   {
@@ -10,13 +9,5 @@ mongoose.connect(
     useFindAndModify: false,
   }
 );
-=======
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
->>>>>>> master
 
 module.exports = mongoose.connection;
